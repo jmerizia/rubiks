e05824bdaadcb270d998d85f4ff80dff9c00e049
Jacob Merizian <jmerizia@vt.edu>
2019-12-07 22:10:57 -0500

    some stuff not sure

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <dlib/bigint.h>
+#include <dlib/dnn.h>
 
 #include "helpers.hpp"
+
+using namespace std;
+using namespace dlib;
 
 /*
  * Given an initial state, and a final state,
@@ -22,6 +25,9 @@ search(
     return v;
 }
 
+double dist(int r, int c) {
+    return sqrt((double)r*r+c*c);
+}
 
 int
 main(void) {
@@ -30,11 +36,66 @@ main(void) {
 
     //std::cout << actions["F"].size() << std::endl;
 
-    State s;
-    State sp = s.apply(actions["B"]);
+    using input_type = matrix<float, 2, 1>;
 
-    print_state(s);
-    print_state(sp);
+    std::vector<input_type> data;
+    std::vector<float> labels;
+    for (int r = 0; r <= 20; r++) {
+        for (int c = 0; c <= 20; c++) {
+            input_type point;
+            point = ((double)r) / 20.0,
+                    ((double)c) / 20.0;
+            data.push_back(point);
+            labels.push_back(dist(r, c) / 30.0);
+        }
+    }
+
+    using net_type = loss_mean_squared<
+                     relu<fc<1,
+                     relu<fc<10,
+                     input<matrix<float, 2, 1
+                     >>>>>>>;
+
+    net_type net;
+
+
+    //trainer.set_synchronization_file("test_sync", std::chrono::seconds(20));
+
+    input_type t;
+    for (int i = 0; i < 100; i++) {
+
+        // create trainer and train:
+        dnn_trainer<net_type> trainer(net);
+        trainer.set_learning_rate(0.01);
+        trainer.set_min_learning_rate(0.01);
+        trainer.set_max_num_epochs(10);
+        //trainer.be_verbose();
+        trainer.train(data, labels);
+
+        // see the error:
+        double total_error = 0.0;
+        int total_tests = 0;
+        for (int r = 0; r <= 20; r++) {
+            for (int c = 0; c <= 20; c++) {
+                t = ((double)r) / 20.0,
+                    ((double)c) / 20.0;
+                double diff = net(t) * 30 - dist(r, c);
+                total_error += diff*diff;
+                total_tests++;
+            }
+        }
+        cout << total_error / total_tests << endl;
+    }
+
+    //net.clean();
+    //serialize("test_network.dat") << net;
+
+
+    //State s;
+    //State sp = s.apply(actions["B"]);
+
+    //print_state(s);
+    //print_state(sp);
 
     return 0;
 }
