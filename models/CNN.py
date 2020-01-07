6c132f7475ef2ccb24ed6d8a76748d604967a682
Jacob Merizian <jmerizia@vt.edu>
2020-01-07 17:07:40 -0500

    run test on home setup

--- models/CNN.py
+++ models/CNN.py
@@ -6,13 +6,24 @@ from __future__ import absolute_import
 import tensorflow as tf
 from tensorflow.keras import Model, layers
 import datetime
+import os
+import numpy as np
+
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+# Set memory to grow little-by-little as needed
+# (because Tensorflow is shit at managing memory)
+for device in tf.config.experimental.list_physical_devices('GPU'):
+    tf.config.experimental.set_memory_growth(device, True)
+
 
 class ConvNet(Model):
 
     def __init__(self):
         super(ConvNet, self).__init__()
 
-        self.conv1 = layers.Conv2D(32, kernel_size=5, activation=tf.nn.relu)
+        self.conv1 = layers.Conv2D(64, kernel_size=5, activation=tf.nn.relu)
         self.maxpool1 = layers.MaxPool2D(2, strides=2)
 
         self.conv2 = layers.Conv2D(64, kernel_size=3, activation=tf.nn.relu)
@@ -23,25 +34,29 @@ class ConvNet(Model):
 
         self.flatten = layers.Flatten()
 
-        self.fc1 = layers.Dense(1024, activation=tf.nn.relu)
-        self.fc2 = layers.Dense(1024, activation=tf.nn.relu)
-        self.fc3 = layers.Dense(512, activation=tf.nn.relu)
-        self.fc4 = layers.Dense(128, activation=tf.nn.relu)
+        self.fc1 = layers.Dense(1000, activation=tf.nn.relu)
+        self.fc2 = layers.Dense(1000, activation=tf.nn.relu)
+        self.fc3 = layers.Dense(1000, activation=tf.nn.relu)
+        self.fc4 = layers.Dense(1000, activation=tf.nn.relu)
+        self.fc5 = layers.Dense(1000, activation=tf.nn.relu)
+
         self.dropout = layers.Dropout(rate=0.5)
 
         self.out = layers.Dense(1)
 
+    @tf.function
     def call(self, x, is_training=False):
-        x = tf.reshape(x, [-1, 12, 12, 1])
-        x = self.conv1(x)
-        x = self.maxpool1(x)
-        x = self.conv2(x)
-        x = self.maxpool2(x)
-        x = self.flatten(x)
+        #x = tf.reshape(x, [-1, 12, 12, 1])
+        #x = self.conv1(x)
+        #x = self.maxpool1(x)
+        #x = self.conv2(x)
+        #x = self.maxpool2(x)
+        #x = self.flatten(x)
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.fc3(x)
         x = self.fc4(x)
+        x = self.fc5(x)
         x = self.dropout(x, training=is_training)
         x = self.out(x)
         return x
@@ -52,10 +67,10 @@ class CNN:
     def __init__(self, fname=None):
         if not fname:
             self.conv_net = ConvNet()
-            self.batch_size    = 512
-            self.display_step  = 20
-            self.learning_rate = 0.001
-            self.epochs        = 800
+            self.batch_size    = 128
+            self.display_step  = 200
+            self.learning_rate = 0.0005
+            self.epochs        = 3000
             self.optimizer = tf.optimizers.Adam(self.learning_rate)
         else:
             self.conv_net = ConvNet()
@@ -78,14 +93,24 @@ class CNN:
             gradients = g.gradient(loss, trainable_variables)
             self.optimizer.apply_gradients(zip(gradients, trainable_variables))
 
+    def profile_graph(self):
+        writer = tf.summary.create_file_writer('logs/traces/')
+        tf.summary.trace_on(graph=True, profiler=True)
+        with writer.as_default():
+            self.conv_net(np.random.rand(144))
+            tf.summary.trace_export(
+                'trace',
+                step=0,
+                profiler_outdir='logs/profiles')
+
     def train(self, train_x, train_y, test_x, test_y):
 
         num_examples = train_x.shape[0]
 
         # Setup tensorboard:
-        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
-        test_log_dir = 'logs/gradient_tape/' + current_time + '/test'
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        train_log_dir = 'logs/' + current_time + '/train'
+        test_log_dir = 'logs/' + current_time + '/test'
         train_summary_writer = tf.summary.create_file_writer(train_log_dir)
         test_summary_writer = tf.summary.create_file_writer(test_log_dir)
 
@@ -126,6 +151,7 @@ class CNN:
             with test_summary_writer.as_default():
                 tf.summary.scalar('loss',     test_loss, step=epoch)
                 tf.summary.scalar('accuracy', test_acc,  step=epoch)
+
 
         print('Finished Training')
 
